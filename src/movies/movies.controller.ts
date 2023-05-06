0971688cc46854d82948ea58feacaca0d3de3413
minwoo129 <key0129mw@naver.com>
2023-05-06 15:46:22 +0900

    movies 함수 추가: getOne

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -1,9 +1,14 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 
 @Controller('movies')
 export class MoviesController {
   @Get()
   getAll() {
     return 'This will return all movies';
+  }
+
+  @Get('/:id')
+  getOne(@Param('id') id: string) {
+    return `This will return one movie with the id: ${id}`;
   }
 }
